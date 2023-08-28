92332be9fb51d32ef359b67ae2623d4546cd873f
aaronguilford <aaron.guilford@fmg.co.nz>
2023-08-28 22:58:55 +1200

    react query

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 // include styles from the ui package
 import "@fmg/ui/styles.css";
+import { QueryClient } from "@tanstack/react-query";
 
 import type { AppProps } from "next/app";
 
