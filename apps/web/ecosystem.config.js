5c95685df80935d6ccbc7f9c1547c503dc955a84
aaronguilford <aaron.guilford@fmg.co.nz>
2023-08-29 10:59:22 +1200

    eadsfdsafdsf

--- apps/web/ecosystem.config.js
+++ apps/web/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "linkaaron2",
         script: "./node_modules/next/dist/bin/next",
-        args: "start",
+        args: "start -p " + (process.env.PORT || 3000),
         watch: false,
         autorestart: true,
       },
