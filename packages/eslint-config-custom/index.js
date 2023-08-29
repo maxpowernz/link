124cfa27e13937de98f2332668cc6af80d968099
aaronguilford <aaron.guilford@fmg.co.nz>
2023-08-29 20:18:41 +1200

    message

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   extends: ['next', 'turbo', 'prettier'],
-  // rules: {
-  //   "@next/next/no-html-link-for-pages": "off",
-  //   "@typescript-eslint/no-unused-vars": "warn",
-  //   "@typescript-eslint/no-explicit-any": "warn",
-  //   "prefer-const": "warn",
-  //   "import/no-named-as-default": 0,
-  // },
-  // parserOptions: {
-  //   babelOptions: {
-  //     presets: [require.resolve('next/babel')],
-  //   },
-  // },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prefer-const": "warn",
+    "import/no-named-as-default": 0,
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
 };
