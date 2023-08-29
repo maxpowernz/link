debdfda6eb028960a48c8412c9576d8aabfe4f21
aaronguilford <aaron.guilford@fmg.co.nz>
2023-08-30 07:15:01 +1200

    message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "linkaaron2",
-        script: ".apps/web/node_modules/next/dist/bin/next",
+        script: "apps/web/node_modules/next/dist/bin/next",
         args: "start -p 8080",
         watch: false,
         autorestart: true,
