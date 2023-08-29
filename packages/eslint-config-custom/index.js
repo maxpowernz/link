63c65d6eafacf872cfb167312e0778a256de7ae1
aaronguilford <aaron.guilford@fmg.co.nz>
2023-08-29 21:23:55 +1200

    message

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,14 +2,12 @@ module.exports = {
   extends: ['next', 'turbo', 'prettier'],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
     "prefer-const": "warn",
     "import/no-named-as-default": 0,
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve('next/babel')],
+      //presets: [require.resolve('next/babel')],
     },
   },
 };
